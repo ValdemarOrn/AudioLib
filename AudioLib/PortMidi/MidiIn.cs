670309cb108f7bb5dea7287e81f91a59a935c168
ValdemarOrn <valdiorn@gmail.com>
2016-07-25 00:58:23 +0100

    improve midi in support

---
+++ AudioLib/PortMidi/MidiIn.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AudioLib.PortMidiInterop
+{
+	public class MidiIn
+	{
+		private const int BufferSize = 128;
+
+		private readonly int deviceId;
+		private readonly PortMidi.PmEvent[] buffer = new PortMidi.PmEvent[BufferSize];
+		private readonly List<byte> sysexMessage;
+		private PortMidi.PmTimeProcPtr timeCallback;
+
+		private IntPtr stream;
+		private volatile bool running;
+		private Thread thread;
+		private bool insideSysexMessage;
+		
+		public event Action<byte[], bool> DataReceived;
+
+		public MidiIn(int deviceId)
+		{
+			sysexMessage = new List<byte>();
+			this.deviceId = deviceId;
+		}
+
+		public bool IsStarted => running;
+
+		public void Start()
+		{
+			if (running)
+				throw new Exception("Input is already started");
+
+			timeCallback = PortMidi.Pt_Time;
+			var err = PortMidi.Pm_OpenInput(out stream, deviceId, (IntPtr)0, 128, timeCallback, (IntPtr)0);
+			running = true;
+			insideSysexMessage = false;
+			this.thread = new Thread(Listen) { IsBackground = true };
+			thread.Start();
+		}
+
+		public void Stop()
+		{
+			running = false;
+			thread.Join();
+		}
+
+		private void Listen()
+		{
+			while (running)
+			{
+				var count = PortMidi.Pm_Read(stream, buffer, BufferSize);
+				for (int i = 0; i < count; i++)
+				{
+					var d = (uint)buffer[i].message;
+					var b0 = (byte)(d & 0xFF);
+					var b1 = (byte)((d & 0xFF00) >> 8);
+					var b2 = (byte)((d & 0xFF0000) >> 16);
+					var b3 = (byte)((d & 0xFF000000) >> 24);
+
+					if (b0 == 0xF0)
+					{
+						insideSysexMessage = true;
+						sysexMessage.Clear();
+					}
+
+					if (insideSysexMessage)
+					{
+						ParseSysex(new[] { b0, b1, b2, b3 });
+						if (!insideSysexMessage && DataReceived != null)
+							DataReceived.Invoke(sysexMessage.ToArray(), true);
+					}
+					else
+					{
+						var output = new[] { b0, b1, b2, b3 };
+						if (DataReceived != null)
+							DataReceived.Invoke(output.ToArray(), false);
+					}
+				}
+
+				Thread.Sleep(1);
+			}
+		}
+		
+		private void ParseSysex(byte[] bytes)
+		{
+			foreach (var b in bytes)
+			{
+				sysexMessage.Add(b);
+
+				if (b == 0xF7)
+				{
+					insideSysexMessage = false;
+					break;
+				}
+			}
+		}
+	}
+}
---
+++ AudioLib/SplineLut/BezierLookup.cs
@@ -0,0 +1,92 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AudioLib.SplineLut
+{
+	public class BezierPoint
+	{
+		public double X;
+		public double Y;
+
+		public double C1x;
+		public double C1y;
+		public double C2x;
+		public double C2y;
+
+		public PointType PointType;
+	}
+
+	public enum PointType
+	{
+		Fixed, // control points mirror around center
+		Uneven, // same derivative, different magnitude
+		Free // completely independent points
+	}
+
+	public class BezierLookup
+	{
+        public double Bias;
+
+		private double[] xs;
+		private double[] ys;
+		private double[] ks;
+        private double max;
+        private double min;
+
+        public BezierLookup()
+        {
+            var data = JsonConvert.DeserializeObject<double[][]>(jsonData);
+            SetData(data[0], data[1], data[2]);
+        }
+
+        /// <summary>
+        /// Arrays must be sorted from lowest x value to highest
+        /// </summary>
+        /// <param name="xs"></param>
+        /// <param name="ys"></param>
+        /// <param name="ks"></param>
+        public SplineInterpolator(double[] xs, double[] ys, double[] ks)
+		{
+            SetData(xs, ys, ks);
+		}
+
+        private void SetData(double[] xs, double[] ys, double[] ks)
+        {
+            this.xs = xs;
+            this.ys = ys;
+            this.ks = ks;
+            this.max = xs.Max();
+            this.min = xs.Min();
+        }
+
+        public void ProcessInPlace(double[] inputs)
+        {
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                var x = inputs[i] + Bias;
+                if (x > max) x = max - 0.00001;
+                if (x < min) x = min + 0.00001;
+                var y = InterpolateNoBias(x);
+                inputs[i] = y;
+            }
+        }
+
+		public double InterpolateNoBias(double x)
+		{
+			var i = 1;
+			while (xs[i] < x)
+				i++;
+			
+			var t = (x - xs[i - 1]) / (xs[i] - xs[i - 1]);
+			
+			var a = ks[i - 1] * (xs[i] - xs[i - 1]) - (ys[i] - ys[i - 1]);
+			var b = -ks[i] * (xs[i] - xs[i - 1]) + (ys[i] - ys[i - 1]);
+
+			var q = (1 - t) * ys[i - 1] + t * ys[i] + t * (1 - t) * (a * (1 - t) + b * t);
+			return q;
+		}
+	}
+}
